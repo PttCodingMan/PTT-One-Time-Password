188b616eb8298f2468db1dcddb93ffa2e88b7a7d
CodingMan <CodingMan@uptt.cc>
2020-05-08 18:10:21 +0800

    Add ptt login, catch close event to logout

--- PTTOTP/util.py
+++ PTTOTP/util.py
@@ -1,12 +1,38 @@
+import sys
+
+from PySide2.QtWidgets import (QLabel, QLineEdit, QPushButton, QApplication,
+                               QVBoxLayout, QDialog)
+from PySide2.QtWidgets import QMessageBox
 
 
 def get_bytes_from_file(filename):
     return open(filename, "rb").read().hex()
 
+
 def hex_byte(hex):
     return bytearray.fromhex(hex)
 
-if __name__ == '__main__':
-    hex = get_bytes_from_file('../PTTOTP.ico')
 
-    print(hex)
+def alert(msg_str):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Information)
+
+    msg.setText(msg_str)
+    # msg.setInformativeText("This is additional information")
+    msg.setWindowTitle("PttOTP")
+    # msg.setDetailedText("The details are as follows:")
+    # msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+    msg.setStandardButtons(QMessageBox.Ok)
+    # msg.buttonClicked.connect(msgbtn)
+
+    retval = msg.exec_()
+
+
+if __name__ == '__main__':
+    # hex = get_bytes_from_file('../PTTOTP_small.png')
+    # print(hex)
+
+    app = QApplication(sys.argv)
+    alert()
+
+    sys.exit()
